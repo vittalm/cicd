a55f1172977f0647f646c053fb75465be197489a
vittal mareddy <mvittalreddy@gmail.com>
2023-10-27 20:37:28 -0400

    modified to make it work in external tomcat

--- src/main/java/com/training/cicd/CicdApplication.java
+++ src/main/java/com/training/cicd/CicdApplication.java
@@ -2,12 +2,17 @@ package com.training.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CicdApplication {
+public class CicdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdApplication.class, args);
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CicdApplication.class);
 	}
 
 }
--- src/main/java/com/training/cicd/controller/ProductController.java
+++ src/main/java/com/training/cicd/controller/ProductController.java
@@ -2,13 +2,14 @@ package com.training.cicd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
     @GetMapping("/all")
-  public String  getAllProducts(){
-        return "All products";
+  public String  getAllProducts(@RequestParam(required = false) String id){
+        return "All products : "+id;
     }
 }
